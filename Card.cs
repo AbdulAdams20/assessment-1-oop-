53b283df5aee620a6c22755e7cd8f29f2cb2ddf9
AbdulAdams20 <127293136+AbdulAdams20@users.noreply.github.com>
2023-03-08 10:01:23 +0100

    Update Card.cs

--- Card.cs
+++ Card.cs
@@ -6,24 +6,25 @@ using System.Threading.Tasks;
 
 namespace CMP1903M_A01_2223
 {
-    public class Card
+    // The Card class represents a single playing card, with a rank and a suit.
+public class Card
+{
+    // Private fields to store the rank and suit of the card.
+    private string rank;
+    private string suit;
+
+    // Constructor to initialize the rank and suit of the card.
+    public Card(string rank, string suit)
     {
-        public enum Suit { Hearts, Diamonds, Clubs, Spades };
-        public enum Rank { Ace = 1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King };
-
-        public Suit CardSuit { get; set; }
-        public Rank CardRank { get; set; }
-
-        public Card(Suit suit, Rank rank)
-        {
-            CardSuit = suit;
-            CardRank = rank;
-        }
-
-        public override string ToString()
-        {
-            return $"{CardRank} of {CardSuit}";
-        }
+        this.rank = rank;
+        this.suit = suit;
     }
+
+    // ToString() method to return a string representation of the card.
+    public override string ToString()
+    {
+        return rank + " of " + suit;
+    }
+}
 
 }
